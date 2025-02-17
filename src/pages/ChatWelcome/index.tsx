00c6c8b35ee21412b873ba765979dee69c36e90e
wangkaiyue <wangky_201012@163.com>
2025-02-17 19:08:27 +0800

    内联对话框基本页面

--- src/pages/ChatWelcome/index.tsx
+++ src/pages/ChatWelcome/index.tsx
@@ -1,5 +1,6 @@
 import './chatWelcome.scss';
 import InputBox from '../../components/InputBox';
+import InlineDialog from '../InlineDialog/index';
 import {
   OpenAIOutlined,
 } from '@ant-design/icons';
@@ -8,11 +9,18 @@ import {
 const ChatWelcome = () => {
 
   return (
-    <div className='chat'>
-      <div className='chat-welcome'>
-        <p className='chat-welcome-title'><OpenAIOutlined /> 我是 LLM-Chat 助手，很高兴见到你！</p>
-        <p className='chat-welcome-content'>我可以帮你写代码、读文件、写作各种创意内容，请把你的任务交给我吧~</p>
-        <InputBox />
+    <div className="chat-container">
+      <InlineDialog />
+      <div className="chat">
+        <div className="chat-welcome">
+          <p className="chat-welcome-title">
+            <OpenAIOutlined /> 我是 LLM-Chat 助手，很高兴见到你！
+          </p>
+          <p className="chat-welcome-content">
+            我可以帮你写代码、读文件、写作各种创意内容，请把你的任务交给我吧~
+          </p>
+          <InputBox />
+        </div>
       </div>
     </div>
   )
