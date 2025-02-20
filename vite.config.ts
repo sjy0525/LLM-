50081db4c2e3abe00543d6c783f8b2a70ab264e8
sjy <1979974477@qq.com>
2025-02-20 11:25:47 +0800

    scss配置问题

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,20 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import autoprefixer from "autoprefixer"
-
+import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src') // 设置路径别名，方便后续引入文件
+    }
+  },
   css: {
     preprocessorOptions: {
       scss: {
         charset: false,
+        additionalData: `@import "@/**/**.scss";` 
       },
     },
     postcss: {
